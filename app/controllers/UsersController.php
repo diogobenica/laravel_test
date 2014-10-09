4d1d6a0ddddde9e4e538dbdb9498ad654ae254f9
Diogo Benicá <diogobenica@gmail.com>
2014-10-09 13:42:58 -0300

    Users.

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,121 @@
+<?php
+
+class UsersController extends Controller {
+
+	/**
+	 * Display a listing of users
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$users = User::all();
+
+		return View::make('users.index', compact('users'));
+	}
+
+	/**
+	 * Show the form for creating a new user
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('users.create');
+	}
+
+	/**
+	 * Store a newly created user in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$rules = array(
+			'name'         => 'required',
+			'password'     => 'required',
+			'full_address' => 'required',
+			'email'        => 'required'
+		);
+		$validator = Validator::make(Input::all(), $rules);
+
+		if ($validator->fails()) {
+			return Redirect::to('users/create')
+				->withErrors($validator);
+		} else {
+			$user = new User;
+			$user->name         = Input::get('name');
+			$user->email        = Input::get('email');
+			$user->password     = Hash::make(Input::get('password'));
+			$user->full_address = Input::get('email');
+			$user->save();
+
+			Session::flash('message', 'Cadastrado com sucesso!');
+			Session::put('authenticated', $user->id);
+
+			return Redirect::to('pizzas');
+		}
+	}
+
+	/**
+	 * Display the specified user.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$user = User::findOrFail($id);
+
+		return View::make('users.show', compact('user'));
+	}
+
+	/**
+	 * Show the form for editing the specified user.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		$user = User::find($id);
+
+		return View::make('users.edit', compact('user'));
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		$user = User::findOrFail($id);
+
+		$validator = Validator::make($data = Input::all(), User::$rules);
+
+		if ($validator->fails())
+		{
+			return Redirect::back()->withErrors($validator)->withInput();
+		}
+
+		$user->update($data);
+
+		return Redirect::route('users.index');
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		User::destroy($id);
+
+		return Redirect::route('users.index');
+	}
+
+}
